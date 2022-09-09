ace59f3bfbf0738b1e7caf44460494c4d8c5c3fa
Hataken <75216370+Hataken999@users.noreply.github.com>
2022-09-09 07:58:57 +0700

    Add files via upload

---
+++ web-tools/youtube-thumbnail-grabber/script.js
@@ -0,0 +1,97 @@
+const videoUrl = document.querySelector(".wrapper input"),
+thumbName = document.querySelector(".thumb-name input"),
+thumbResolution = document.querySelector(".select-menu select"),
+grabBtn = document.querySelector(".grab-btn"),
+downloadBtn = document.querySelector(".download-btn"),
+preview = document.querySelector(".preview");
+let videoId, thumbnailUrl;
+let thumbnailBaseUrl = "https://img.youtube.com/vi/";
+let online = navigator.onLine;
+
+videoUrl.addEventListener("focus", () => {
+  if (videoUrl.value) {
+    videoUrl.select();
+  }
+})
+videoUrl.addEventListener("click", () => {
+  if (videoUrl.value) {
+    videoUrl.select();
+  }
+})
+/*videoUrl.addEventListener("touchstart", () => {
+  if (videoUrl.value) {
+    videoUrl.select();
+  }
+})*/
+
+videoUrl.addEventListener("keyup", () => {
+  if (!videoUrl.value) {
+    preview.classList.remove("show");
+  }
+})
+
+grabBtn.addEventListener("click", () => {
+  if (!online) {
+    alert("Kamu sedang offline, tunggu sampai online lagi untuk menggunakan tools ini.");
+    return;
+  }
+  
+  if (!videoUrl.value.includes("music.youtube.com") && videoUrl.value.includes("youtu.be")) {
+    videoId = videoUrl.value.split("/")[3];
+  } else if(videoUrl.value.includes("music.youtube")) {
+    videoId = videoUrl.value.split(/[=&]+/)[1];
+  } else if(videoUrl.value.includes("youtube.com")) {
+    videoId = videoUrl.value.split(/[/?]/)[4];
+    console.log(videoId);
+  } else {
+    imgError();
+    return;
+  }
+  const thumbImg = document.getElementById("thumb-img");
+
+  if (videoId) {
+    thumbnailUrl = thumbnailBaseUrl + videoId + "/" + thumbResolution.value + ".jpg";
+    thumbImg.src = thumbnailUrl;
+    grabBtn.innerText = "Grabbing Thumbnail...";
+    thumbImg.addEventListener("load", () => {
+      grabBtn.innerText = "Grab";
+      preview.classList.add("show");
+    })
+  } else {
+    imgError();
+  }
+})
+
+downloadBtn.addEventListener("click", () => {
+
+  const myRequest = new Request(thumbnailUrl);
+
+  fetch(myRequest)
+  .then((response) => response.blob())
+  .then((myBlob) => {
+    const objectURL = URL.createObjectURL(myBlob),
+    link = document.createElement("a");
+    link.href = objectURL;
+
+    if (thumbName.value) {
+      link.download = `${thumbName.value} - Hataken Project`;
+    } else {
+      link.download = `${thumbResolution.value} - Hataken Project`;
+    }
+    link.click();
+    URL.revokeObjectURL(objectURL);
+    link.remove();
+  });
+
+})
+
+function imgError() {
+  alert("Gagal mengambil thumbnail! Coba cek URL yang diinput valid atau tidak.");
+}
+
+function connection() {
+  if (!online) {
+    alert("Kamu sedang offline, tunggu sampai online lagi untuk menggunakan tools ini.");
+    return;
+  }
+}
