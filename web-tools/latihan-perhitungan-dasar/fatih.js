54fa519ac67d8fe07e6348703ff625af8485e0d4
Hataken <75216370+Hataken999@users.noreply.github.com>
2022-11-27 05:13:23 +0700

    Update fatih.js

--- web-tools/latihan-perhitungan-dasar/fatih.js
+++ web-tools/latihan-perhitungan-dasar/fatih.js
@@ -4,14 +4,19 @@ tanyaBtn = document.querySelector(".tanyaBtn"),
 jwbBtn = document.querySelector(".jwbBtn"),
 gantiMetodeBtn = document.querySelector(".extra button"),
 customAlert = document.querySelector(".custom-alert");
-let metode, num, jawaban, timeout;
+let metode, num, jawaban, timeoutMsg;
 
+  alert("Disarankan menggunakan Chrome/browser modern lainnya yang versi terbaru!");
+  
 tanyaBtn.addEventListener("click", () => {
+  try {
   let maxNum = 1 + num;
   console.log(maxNum);
   let bil1 = Math.floor(Math.random() * maxNum),
   bil2 = Math.floor(Math.random() * maxNum);
-  timeout = "";
+  timeoutMsg = "";
+  
+  if(!metode) throw new Error("Pilih metode perhitungan terlebih dahulu!");
 
   if (metode == "kali") {
     jawaban = bil1 * bil2;
@@ -19,7 +24,7 @@ tanyaBtn.addEventListener("click", () => {
     p2.innerText = '';
     p1.innerText = `${bil1} x ${bil2} = ?`;
   } else if (metode == "bagi") {
-    jawaban = (isNaN(bil1 / bil2)) ? "Tidak dapat dibagi" : bil1 / bil2;
+    jawaban = (isNaN(bil1 / bil2)) ? "Tidak dapat dibagi": bil1 / bil2;
     console.log(jawaban)
 
     p2.innerText = '';
@@ -35,15 +40,24 @@ tanyaBtn.addEventListener("click", () => {
     p2.innerText = '';
     p1.innerText = `${bil1} - ${bil2} = ?`;
   }
-  
+
   /*setTimeout(function() {
-    timeout = "(Kelamaan)";
+    timeoutMsg = "(Kelamaan)";
     jwbBtn.click();
   }, 20000);*/
-  
+  } catch (e) {
+    customAlertPopup(e.message);
+  }
+
 });
 jwbBtn.addEventListener("click", () => {
-  p2.innerText = `Jawabannya adalah ${jawaban} ${timeout}`;
+  try {
+    if (jawaban == null) throw new Error("Jawaban masih kosong!");
+    
+    p2.innerText = `Jawabannya adalah ${jawaban} ${timeoutMsg}`;
+  } catch (e) {
+    customAlertPopup(e.message);
+  }
 });
 
 const judul = (metode, angka) => {
@@ -63,7 +77,9 @@ const inputMetode = () => {
 
     if (!angkaMaks) throw new Error("Angka Maksimal kosong!");
 
-    angkaMaks = parseInt(angkaMaks);
+    angkaMaks = (angkaMaks.includes("/^[a-zA-Z]+/")) ? NaN : parseInt(angkaMaks);
+    
+    console.log(angkaMaks);
 
     if (isNaN(angkaMaks)) throw new Error("'Angka Maksimal' hanya boleh diisi dengan angka!");
 
@@ -90,23 +106,24 @@ const inputMetode = () => {
     }
   } catch (e) {
     customAlertPopup(e.message);
-    window.close();
+    //window.close();
   }
 }
 
 const customAlertPopup = error => {
   const desc = document.querySelector(".description-alert p");
-  customAlert.classList.toggle("show");
+  customAlert.classList.add("show");
   desc.innerText = error;
 
   setTimeout(function() {
-    customAlert.classList.toggle("show");
+    customAlert.classList.remove("show");
   }, 5000);
 }
 
 const clear = () => {
   p1.innerText = "";
   p2.innerText = "";
+  jawaban = null;
 }
 
 gantiMetodeBtn.addEventListener("click", () => {
