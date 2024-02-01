2fe4dd9c22d87599ffc2f1a0a105606d0dd556cb
Hataken999 <faizuzairkhaerudin@gmail.com>
2024-02-02 03:35:20 +0700

    Perbarui: Youtube Thumbnail Grabber tool

--- web-tools/youtube-thumbnail-grabber/script.js
+++ web-tools/youtube-thumbnail-grabber/script.js
@@ -3,13 +3,22 @@ thumbName = document.querySelector(".thumb-name input"),
 thumbResolution = document.querySelector(".select-menu select"),
 grabBtn = document.querySelector(".grab-btn"),
 downloadBtn = document.querySelector(".download-btn"),
-preview = document.querySelector(".preview"),
-formEl = document.querySelector("form");
+preview = document.querySelector(".preview-wrapper"),
+formEl = document.querySelector("form"),
+thumbAnchor = document.querySelector('.thumb-img a');
 const thumbImg = document.getElementById("thumb-img");
 let videoId, thumbnailUrl;
 let thumbnailBaseUrl = "https://img.youtube.com/vi/";
 const corsProxy = "https://bots.hataken.eu.org/api/download/ytt";
 const notyf = new Notyf({duration: 5_000, position: {y: 'bottom'}, dismissible: true});
+const $gallery = new SimpleLightbox(thumbAnchor, {});
+
+tippy('#thumbName', {
+  content: "Secara default nama file akan sesuai format '{ID-VIDEO}_{NAMA-RESOLUSI} - Hataken Project' jika kolom ini tidak diisi.",
+  placement: 'auto',
+  arrow: true
+});
+
 
 videoUrl.addEventListener("focus", () => {
   if (videoUrl.value) {
@@ -28,43 +37,21 @@ videoUrl.addEventListener("click", () => {
 })*/
 
 videoUrl.addEventListener("keyup", () => {
+  if (videoUrl.checkValidity()) grabBtn.disabled = false;
   if (!videoUrl.value) {
     preview.classList.remove("show");
+    grabBtn.disabled = true;
   }
 })
 
 grabBtn.addEventListener("click", e => {
 e.preventDefault();
   
- if (videoUrl.value.includes("youtu.be")) {
-  // Handle youtu.be URLs
-  videoId = videoUrl.value.split("/")[3].split('?')[0];
-} else if (videoUrl.value.includes("music.youtube.com")) {
-  // Handle music.youtube.com URLs
-  const params = new URLSearchParams(videoUrl.value.split('?')[1]);
-  videoId = params.get('v');
-} else if (videoUrl.value.includes("youtube.com")) {
-  // Handle youtube.com URLs
-  const urlParams = new URLSearchParams(videoUrl.value.split('?')[1]);
-  videoId = urlParams.get('v') || videoUrl.value.split(/[/?]/)[4];
-} else {
-  // Handle other cases or display an error
-  imgError();
-  return;
-}
+grabThumb();
+})
 
-console.log("Video ID:", videoId);
-  if (videoId) {
-    thumbnailUrl =  thumbnailBaseUrl + videoId + "/" + thumbResolution.value + ".jpg";
-    thumbImg.src = thumbnailUrl;
-    grabBtn.innerText = "Grabbing Thumbnail...";
-    thumbImg.addEventListener("load", () => {
-      grabBtn.innerText = "Grab";
-      preview.classList.add("show");
-    })
-  } else {
-    imgError();
-  }
+thumbResolution.addEventListener('change', () => {
+  grabThumb(false);
 })
 
 downloadBtn.addEventListener("click", e => {
@@ -72,6 +59,7 @@ e.preventDefault();
 
   downloadBtn.innerText = "Downloading...";
   downloadBtn.disabled = true;
+  downloadBtn.classList.add('disabled');
 
 /*const headers = headers = {
   'Origin': 'https://img.youtube.com',
@@ -118,9 +106,13 @@ headers.set('Origin', 'https://img.youtube.com');*/
       link.remove();
       downloadBtn.innerText = "Download Thumbnail";
       downloadBtn.disabled = false;
+      downloadBtn.classList.remove('disabled');
       notyf.success('Berhasil mengunduh thumbnail');
     }).catch( e => {
       console.log(e);
+      downloadBtn.innerText = "Download Thumbnail";
+      downloadBtn.disabled = false;
+      downloadBtn.classList.remove('disabled');
       notyf.error('Gagal mengunduh thumbnail, laporin aja cuy..');
     });
 
@@ -155,9 +147,44 @@ function imgError() {
   // alert("Gagal mengambil thumbnail! Coba cek URL yang diinput valid atau tidak.");
 }
 
-function connection() {
-  if (!online) {
-    notyf.error("Kamu sedang offline, tunggu sampai online lagi untuk menggunakan tools ini.");
+function grabThumb(isError = true) {
+  if (videoUrl.value.includes("youtu.be")) {
+    // Handle youtu.be URLs
+    videoId = videoUrl.value.split("/")[3].split('?')[0];
+  } else if (videoUrl.value.includes("music.youtube.com")) {
+    // Handle music.youtube.com URLs
+    const params = new URLSearchParams(videoUrl.value.split('?')[1]);
+    videoId = params.get('v');
+  } else if (videoUrl.value.includes("youtube.com")) {
+    // Handle youtube.com URLs
+    const urlParams = new URLSearchParams(videoUrl.value.split('?')[1]);
+    videoId = urlParams.get('v') || videoUrl.value.split(/[/?]/)[4];
+  } else {
+    // Handle other cases or display an error
+    if (isError) imgError();
     return;
   }
+  
+  console.log("Video ID:", videoId);
+    if (videoId) {
+      thumbnailUrl =  thumbnailBaseUrl + videoId + "/" + thumbResolution.value + ".jpg";
+      thumbAnchor.href = thumbnailUrl;
+      thumbImg.src = thumbnailUrl;
+      grabBtn.innerText = "Grabbing Thumbnail...";
+      grabBtn.disabled = true;
+      grabBtn.classList.add('disabled');
+      thumbImg.addEventListener("load", () => {
+        grabBtn.innerText = "Grab";
+        grabBtn.disabled = false;
+        grabBtn.classList.remove('disabled');
+        preview.classList.add("show");
+        tippy('#previewHelp', {
+          content: "Jika gambar thumbnail menampilkan thumbnail placeholder youtube (yang berwarna abu-abu), maka coba ganti resolusinya satu-persatu.",
+          placement: 'auto',
+          arrow: true
+        });
+      })
+    } else {
+      imgError();
+    }
 }
